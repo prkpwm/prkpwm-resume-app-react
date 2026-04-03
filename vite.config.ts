26d8b0909b1783a79895f2b9eb6c60f290f48703
prkpwm <prkpwm.dev@gmail.com>
2026-04-03 19:29:02 +0700

    Build as single HTML file using vite-plugin-singlefile

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   base: '/prkpwm-resume-app-react/',
+  build: {
+    assetsInlineLimit: 100_000_000,
+    cssCodeSplit: false,
+    codeSplitting: false,
+  },
 })
